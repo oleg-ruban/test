a870cfdf946ff5fb904619e2705dfe79e08cba16
Oleg Ruban <o.ruban@temabit.com>
2026-02-23 20:41:29 +0200

    fix lab6_14

--- lab6_14.cpp
+++ lab6_14.cpp
@@ -1,76 +1,34 @@
 #include <iostream>
-#include <ctime>
 
 using namespace std;
 
-const int N = 200;
+int recursiveCalls = 0;
 
-// --------------------
-// Рекурсивна сума цифр
-// --------------------
-int sumOfDigits(int number) {
-    if (number == 0)
-        return 0;
-    return number % 10 + sumOfDigits(number / 10);
+int maxDigit(unsigned long long n) {
+    recursiveCalls++;  // рахуємо виклик
+
+    // Базовий випадок: якщо число однозначне
+    if (n < 10) {
+        return n;
+    }
+
+    // Рекурсивний випадок
+    int lastDigit = n % 10;
+    int maxOfRest = maxDigit(n / 10);
+
+    return (lastDigit > maxOfRest) ? lastDigit : maxOfRest;
 }
 
-// --------------------------------------
-// Рекурсивний пошук мінімального елемента
-// --------------------------------------
-int findMinIndex(int arr[], int size, int start, int currentMin) {
-    if (start >= size)
-        return currentMin;
-
-    int sumStart = sumOfDigits(arr[start]);
-    int sumMin = sumOfDigits(arr[currentMin]);
-
-    if (sumStart < sumMin ||
-       (sumStart == sumMin && arr[start] < arr[currentMin]))
-        currentMin = start;
-
-    return findMinIndex(arr, size, start + 1, currentMin);
-}
-
-// --------------------------------------
-// Рекурсивне сортування вибором
-// --------------------------------------
-void recursiveSelectionSort(int arr[], int size, int index = 0) {
-    if (index >= size - 1)
-        return;
-
-    int minIndex = findMinIndex(arr, size, index + 1, index);
-
-    if (minIndex != index)
-        swap(arr[index], arr[minIndex]);
-
-    recursiveSelectionSort(arr, size, index + 1);
-}
-
-// --------------------
-// Головна функція
-// --------------------
 int main() {
-    int arr[N];
+    unsigned long long N;
 
-    // Заповнення масиву числами 1..N
-    for (int i = 0; i < N; i++)
-        arr[i] = i + 1;
+    cout << "Enter natural number N: ";
+    cin >> N;
 
-    // Замір часу
-    clock_t startTime = clock();
+    int result = maxDigit(N);
 
-    recursiveSelectionSort(arr, N);
-
-    clock_t endTime = clock();
-
-    double seconds = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-
-    // Вивід відсортованого масиву
-    cout << "Sorted array:\n";
-    for (int i = 0; i < N; i++)
-        cout << arr[i] << " ";
-
-    cout << "\n\nExecution time: " << seconds << " seconds\n";
+    cout << "Maximum digit: " << result << endl;
+    cout << "Recursive calls: " << recursiveCalls << endl;
 
     return 0;
 }
